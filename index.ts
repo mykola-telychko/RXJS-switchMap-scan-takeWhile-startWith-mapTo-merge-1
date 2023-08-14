707cced3bf77f3abea6aa6f6f6c363f6e5972418
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 20:25:45 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-// RxJS v6+ 
+// RxJS v6+
 import { interval, fromEvent, merge, empty } from 'rxjs';
 import { switchMap, scan, takeWhile, startWith, mapTo } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/creation/empty
+// Example 1: empty immediately completes
+
 const countdownSeconds = 10;
-const setHTML = id => val => (document.getElementById(id).innerHTML = val);
+const setHTML = (id) => (val) => (document.getElementById(id).innerHTML = val);
 const pauseButton = document.getElementById('pause');
 const resumeButton = document.getElementById('resume');
 const interval$ = interval(1000).pipe(mapTo(-1));
@@ -15,8 +18,8 @@ const timer$ = merge(pause$, resume$)
   .pipe(
     startWith(true),
     // if timer is paused return empty observable
-    switchMap(val => (val ? interval$ : empty())),
+    switchMap((val) => (val ? interval$ : empty())),
     scan((acc, curr) => (curr ? curr + acc : acc), countdownSeconds),
-    takeWhile(v => v >= 0)
+    takeWhile((v) => v >= 0)
   )
   .subscribe(setHTML('remaining'));
